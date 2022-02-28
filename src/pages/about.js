21810d68fffa6755a2c7db96f6f9a13bf56b419e
husseyhh <56592096+husseyhh@users.noreply.github.com>
2022-02-28 14:03:01 +0530

    removed boiler plate and add new pages

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Layout from '../components/layout'
+
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>This is the about me page for the website.</p>
+    </Layout>
+  )
+}
+
+export default AboutPage;
